21bc9e98140985d63f68d4bd2c21efb3da41c3f4
Sebastien Kempf <s.kempf@live.com>
2021-09-09 15:16:52 +0200

    handle animations and rewrite about me section

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,36 @@
-window.addEventListener("scroll", event => {
-  let mainNavLinks = document.querySelectorAll("nav ul li a");
+let header = document.querySelector("header");
+let nav = document.querySelector("nav");
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let arrow = document.querySelector("arrow");
+let aside = document.querySelector("aside");
+let toTop = document.querySelector(".to-top");
 
+nav.style.display = "none";
+nav.style.visibility = "hidden";
+aside.style.visibility = "hidden";
+toTop.style.visibility = "hidden";
+
+window.addEventListener("scroll", event => {
   let fromTop = window.scrollY;
+
+  if (fromTop >= window.innerHeight*0.9) {
+    header.style.opacity = 0;
+    nav.style.display = "flex";
+    nav.style.visibility = "visible";
+    toTop.style.visibility = "visible";
+    toTop.style.opacity = 1;
+  } else {
+    header.style.opacity = 1;
+    nav.style.display = "none";
+    toTop.style.opacity = 0;
+  }
+
+  if (fromTop >= window.innerHeight/1.5) {
+    aside.style.visibility = "visible";
+    aside.style.opacity = 1;
+  } else {
+    aside.style.opacity = 0;
+  }
 
   mainNavLinks.forEach(link => {
     let section = document.querySelector(link.hash);
